23dde28d98444a3b148911ecaf7a623cf8b1bb88
ColetteN <k00212038@student.lit.ie>
2017-03-27 15:08:18 +0100

    Update

--- script/script.js
+++ script/script.js
@@ -5,7 +5,32 @@ $(document).ready(function(){
 		$( "#slider" ).slider();
 	  } );
 	
-	  $( function() {
-    	$( "#button_regular" ).resizable();
-  		} );
+	$( function() {
+		$( "#button_regular" ).resizable();
+		} );
+	
+//		var buttonRegular = $('#button_regular').height();
+//		$(function(){
+//			$('#button_regular').on(function(){
+//				$(this).animate({height: '400', width: '400'}, 'slow');
+//			}).mouseleave(function(){
+//				$(this).animate({height: buttonRegular, width: buttonRegular},'slow');
+//			});
+//		});
+	
+//		function reCenterButton() {
+//		$("#button_regular").position({
+//			"my": "center center",
+//			"at": "center center",
+//			"of": $("#button_box")
+//		});
+//	};
 });
+//	$(function(){
+//	  // Bind the swipeHandler callback function to the swipe event on div.box
+//	  $( "#button_regular" ).on( "swipe", swipeHandler );
+//
+//	  // Callback function references the event target and adds the 'swipe' class to it
+//	  function swipeHandler( event ){
+//		$( event.target ).addClass( "swipe" );
+//	  }

00182b99407f3cbcfe761b088d36ac72ce8515a7
ColetteN <k00212038@student.lit.ie>
2017-03-31 12:22:22 +0100

    Mobile styling

--- script/script.js
+++ script/script.js
@@ -1,47 +1,32 @@
 //Pretty Buttons JavaScript; Colette Nash K00212038
 
 $(document).ready(function(){  
+	//creates the button and puts an initial text on it
 	$( function() {
-		$( "#slider" ).slider();
-	  } );
-	
-	$( function() {
-		$( "#button_regular" ).button();
+		$("#button_regular").text("Click Me");
 		} );
+	//Dynamically changes the text on the button as you type using the keyup() method
+		$("#button_text").keyup(function(){
+			var valText = $(this).val();
+			$("#button_regular").text(valText);
+		})
+		.keyup();
+	//Changing the border on the button using the slider
+		
+//		$("#slider").slider({
+//     change: function (event, ui) {
+//          $("#vardiv").css("width", ui.value + "%");
+//     }
+//	});
 	
+	//Changes the size of the button by dragging with the mouse or by touch using the resizable() method
 	$( function() {
 		$( "#button_regular" ).resizable();
 		} );
+	
+	
 });	
 
 
 	
 	
-//		var buttonRegular = $('#button_regular').height();
-
-//		$(function(){
-//			$('#button_regular').on(function(){
-//				$(this).animate({height: '400', width: '400'}, 'slow');
-//			}).mouseleave(function(){
-//				$(this).animate({height: buttonRegular, width: buttonRegular},'slow');
-//			});
-//		});
-
-
-
-//		function reCenterButton() {
-//		$("#button_regular").position({
-//			"my": "center center",
-//			"at": "center center",
-//			"of": $("#button_box")
-//		});
-//	};
-
-//	$(function(){
-//	  // Bind the swipeHandler callback function to the swipe event on div.box
-//	  $( "#button_regular" ).on( "swipe", swipeHandler );
-//
-//	  // Callback function references the event target and adds the 'swipe' class to it
-//	  function swipeHandler( event ){
-//		$( event.target ).addClass( "swipe" );
-//	  }

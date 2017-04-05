5b944f637ae42b8897623016c0f6efd48cc7cd40
ColetteN <k00212038@student.lit.ie>
2017-04-05 19:26:50 +0100

    Changed html as mobile jquery would not work

--- script/script.js
+++ script/script.js
@@ -3,28 +3,51 @@
 $(document).ready(function(){  
 	//creates the button and puts an initial text on it
 	$( function() {
-		$("#button_regular").text("Click Me");
+		$("#button_regular").button().text("Click Me");
 		} );
+	
 	//Dynamically changes the text on the button as you type using the keyup() method
-		$("#button_text").keyup(function(){
+		$("#text").keyup(function(){
 			var valText = $(this).val();
 			$("#button_regular").text(valText);
 		})
 		.keyup();
-	//Changing the border on the button using the slider
-		
-//		$("#slider").slider({
-//     change: function (event, ui) {
-//          $("#vardiv").css("width", ui.value + "%");
-//     }
-//	});
 	
 	//Changes the size of the button by dragging with the mouse or by touch using the resizable() method
 	$( function() {
 		$( "#button_regular" ).resizable();
 		} );
-	
-	
+
+//Changing the border on the button using the slider		
+		$("#radius").slider({
+			animate:"fast",
+			value:50,
+			min:0,
+			max:100,
+			step:2,
+			slide:changeRadius
+    });
+		function changeRadius (event, slider) {
+         $("#button_regular").css('border-radius', slider.value + '%');
+		}
+		
+//Changing the size of the button using the slider		
+		$("#size").slider({
+			animate:"fast",
+			value:50,
+			min:0,
+			max:100,
+			step:2,
+			slide:changeSize
+    });
+		function changeSize (event, slider) {
+         $("#button_regular").css('width', slider.value + '%');
+		 $("#button_regular").css('height', slider.value + '%');
+		}
+		
+		//function update(jscolor) {
+//    $('#button_regular').css("color", "#" + jscolor);
+//	}
 });	
 
 
